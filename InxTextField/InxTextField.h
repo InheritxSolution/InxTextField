d9b6e5bbfcf3314ef2bd4f81f135da73515e6e0d
InheritX Solution <nimesh@inheritx.com>
2021-03-12 15:47:43 +0530

    initial framework commit

---
+++ InxTextField/InxTextField.h
@@ -0,0 +1,18 @@
+//
+//  InxTextField.h
+//  InxTextField
+//
+//  Created by Inx-MacMini-i3 on 12/03/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for InxTextField.
+FOUNDATION_EXPORT double InxTextFieldVersionNumber;
+
+//! Project version string for InxTextField.
+FOUNDATION_EXPORT const unsigned char InxTextFieldVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <InxTextField/PublicHeader.h>
+
+
---
+++ InxTextField/Source/UITextField+fixCaretPosition.swift
@@ -0,0 +1,14 @@
+
+// MARK: - UITextField extension
+
+import UIKit
+
+extension UITextField {
+    /// Moves the caret to the correct position by removing the trailing whitespace
+    func fixCaretPosition() {
+        let beginning = beginningOfDocument
+        selectedTextRange = textRange(from: beginning, to: beginning)
+        let end = endOfDocument
+        selectedTextRange = textRange(from: end, to: end)
+    }
+}
